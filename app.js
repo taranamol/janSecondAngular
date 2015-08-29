7a3cbc6a5b6fab577751b07717978e15eae9c4c6
taranamol <tarskaur@gmail.com>
2015-08-29 11:59:35 -0700

    post syntax added to mainctrl

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 angular.module('janSecond', [])
-    .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+    
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.posts = [];
+
+    $scope.post = {};
+  
+    $http.get('/')
+      .then(function(response) {
+        $scope.posts = response.data;
+      });
+
+      $scope.createPost = function() {
+        $http.post('/', $scope.post)
+          .then(function(response) {
+            var newPost = response.data;
+            $scope.post = {};
+            $scope.post.push(newPost);
+          });
+      };
   }])
 
+
 ;
+
+
+
 
