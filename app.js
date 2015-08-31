976366977560dffa5cd175cbe0f0ce986234af42
taranamol <tarskaur@gmail.com>
2015-08-30 22:47:27 -0700

    with post working and design changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('janSecond', [])
+angular.module('janSecond', ['ngMap'])
     
   .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
@@ -6,20 +6,46 @@ angular.module('janSecond', [])
 
     $scope.post = {};
   
-    $http.get('/')
-      .then(function(response) {
-        $scope.posts = response.data;
-      });
-
-      $scope.createPost = function() {
-        $http.post('/', $scope.post)
-          .then(function(response) {
-            var newPost = response.data;
-            $scope.post = {};
-            $scope.post.push(newPost);
-          });
-      };
+  $scope.createPost = function() {
+      $scope.posts.push($scope.post);
+      $scope.post = {};
+    };
   }])
+
+  .directive('currentWeather', function() {
+  return {
+    restrict: 'AE',
+    //how can you use this in the html - depends on how it is put in the html. 
+    //if it was "a" or "ae" - then it have to be be in a div
+    scope: {
+      city: '@'
+    },
+    //our directive has it's own directive - it's an isolated scope
+    //the @ sign means we can get the value of the string outside & use it
+    
+    template: '<div class="current-weather"><h4>Weather for {{city}}</h4>{{weather.main.temp}}</div>',
+    //how the text is displaying on the page
+
+    // templateUrl: 'templates/current-weather-template.html',
+    //transclude: true,
+    controller: ['$scope', '$http',
+      function ($scope, $http) {
+        var url = "http://api.openweathermap.org/data/2.5/weather?mode=json&cnt=7&units=imperial&callback=JSON_CALLBACK&q="
+        $scope.getWeather = function(city) {
+          //has a function of getweather
+          $http({ method: 'JSONP', url: url + city })
+            .success(function(data) {
+              $scope.weather = data;
+               console.log(data);
+            });
+        }
+    }],
+    link: function (scope, element, attrs) {
+      scope.weather = scope.getWeather(attrs.city);
+      //bringing it all together - connecting the getweather & with template, etc.
+    }
+  }
+});
 
 
 ;
